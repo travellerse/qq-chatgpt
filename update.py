442840b9adee9ce7b08a84109d0e6d29a2adff69
a1394332633 <1394332633@qq.com>
2022-12-15 22:59:45 +0800

    实现语义化版本号比较

--- update.py
+++ update.py
@@ -2,6 +2,30 @@ import os
 import time
 import requests
 import zipfile
+import re
+
+
+def compare_version(v1, v2):
+    """
+    :return: 如果v1>v2，返回1；如果v1=v2，返回0；如果v1<v2，返回-1
+    """
+    # 通过正则表达式将版本号分割成列表
+    v1_list = re.split(r'[.-]', v1[1:])
+    v2_list = re.split(r'[.-]', v2[1:])
+
+    # 比较版本号列表的每一项，如果当前项不相同，则返回比较结果
+    for i in range(min(len(v1_list), len(v2_list))):
+        if v1_list[i] != v2_list[i]:
+            if int(v1_list[i]) > int(v2_list[i]):
+                return 1
+            else:
+                return -1
+
+    if(len(v1_list)!= len(v2_list)):
+        return -1 if len(v1_list)>len(v2_list) else 1
+    # 如果所有项都相同，则返回0
+    return 0
+
 
 def check_update():
     with open("version.txt", "r") as f:
@@ -11,9 +35,8 @@ def check_update():
         'https://github.com/travellerse/qq-chatgpt/raw/master/version.txt', verify=False)
     new_version = response.text
 
-    # 判断本地版本号与服务器版本号是否一致
-    if version != new_version:
-        # 如果不一致，则下载最新版本的软件
+    # 判断本地版本号与服务器版本号
+    if compare_version(version, new_version) == -1:
         download_update(new_version)
         return True
     else:
@@ -21,9 +44,8 @@ def check_update():
         print('您的软件已是最新版本！')
         return False
 
+
 # 定义下载更新的函数
-
-
 def download_update(version):
     # 下载最新版本的软件
     filename = 'qq-chatgpt-'+version+'.zip'
